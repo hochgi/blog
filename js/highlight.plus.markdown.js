0582e8caec08b29b1fa9e05529273c100e42d076
Gilad Hoch <gilad.hoch@thomsonreuters.com>
2017-07-06 08:27:10 +0300

    finished a new post + minor css adjustments

--- js/highlight.plus.markdown.js
+++ js/highlight.plus.markdown.js
@@ -8,6 +8,12 @@ $(document).ready(function () {
     element.innerHTML = formatted;
     $(element).find('code').each(function(i, block) {
       hljs.highlightBlock(block);
+      /* if inline, element style should be:
+         display: inline;
+         border-radius: 5px;
+         padding: 3px !important;
+         box-shadow: 1px 1px 4px #666;
+      */
     });
     $(element).css('display','inline-block');
   });
