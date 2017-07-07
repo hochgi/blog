cb1632094aa09f2f721bd3cf2d1373da1b779d5a
Gilad Hoch <gilad.hoch@thomsonreuters.com>
2017-07-07 09:51:51 +0300

    modified when higlighting takes place

--- js/highlight.plus.markdown.js
+++ js/highlight.plus.markdown.js
@@ -1,3 +1,27 @@
+var converter = null;
+setTimeout(function() {
+  blogger.ui().configure().view();
+}, 0);
+$(window.blogger.ui()).on(&#39;viewitem&#39;, function (event, post, element) {
+  $(&quot;.markdown&quot;).each(function(index, element) {
+    if($(element).css(&#39;display&#39;) == &#39;none&#39;) {
+      if(!converter) {
+        var converter = new Markdown.Converter();
+        Markdown.Extra.init(converter);
+      }
+      var md = element.textContent;
+      var formatted = converter.makeHtml(md);
+      element.textContent = &quot;&quot;;
+      element.innerHTML = formatted;
+      $(element).find(&#39;code:not(.inline)&#39;).each(function(i, block) {
+        hljs.highlightBlock(block);
+      });
+      $(element).css(&#39;display&#39;,&#39;inline-block&#39;);
+    }
+  });
+});
+
+/*
 $(document).ready(function () {
   var converter = new Markdown.Converter();
   Markdown.Extra.init(converter);
@@ -12,3 +36,4 @@ $(document).ready(function () {
     $(element).css('display','inline-block');
   });
 });
+*/
